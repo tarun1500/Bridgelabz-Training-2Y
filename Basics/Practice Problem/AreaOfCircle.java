25252aeba630d88662c189a8360c98ab3a0695c0
tarun1500 <tarunkumarsingh8650@gmail.com>
2026-08-03 21:36:43 +0530

    [Tarun Kumar Singh] AreaOfCircle

---
+++ Basics/Practice Problem/AreaOfCircle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AreaOfCircle {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double radius=sc.nextDouble();
+        double area=Math.PI*radius*radius;
+        System.out.println("Area = "+area);
+
+        sc.close();
+    }
+}
