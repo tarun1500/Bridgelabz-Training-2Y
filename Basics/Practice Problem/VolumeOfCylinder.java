ecdb775e8574532cf69371071dec4e9488f539e9
tarun1500 <tarunkumarsingh8650@gmail.com>
2026-08-03 21:57:33 +0530

    [Tarun Kumar Singh] VolumeOfCylinder

---
+++ Basics/Practice Problem/VolumeOfCylinder.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class VolumeOfCylinder {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double radius=sc.nextDouble();
+        double height=sc.nextDouble();
+        double volume=Math.PI*radius*radius*height;
+        System.out.println("Volume = "+volume);
+
+        sc.close();
+    }
+}
